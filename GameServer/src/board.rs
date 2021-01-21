aa6a96d6779c983fc07944071b729b6adc0f2415
jtochikawa <jtochikawa@DESKTOP-82NDN20.localdomain>
2021-01-21 12:40:56 +0900

    update board.rs and game.rs

--- GameServer/src/board.rs
+++ GameServer/src/board.rs
@@ -7,21 +7,27 @@ impl _Board {
     pub fn _init() -> _Board {
         _Board {
             b: [[0; 9]; 9],
-            king: (5, 5),
+            king: (4, 4),
         }
     }
 
     pub fn show_board(&self) {
+        let mut cnt:i32 = 0;
         for _row in &self.b {
             for _i in 0.._row.len() {
-                print!("| {} ", _row[_i]);
+                if self.king.0 == _i as i32 && self.king.1 == cnt {
+                    print!("| K ");
+                } else {
+                    print!("| {} ", _row[_i]);
+                }
             }
             println!("|");
+            cnt += 1;
         }
     }
 
-    pub fn set_board(&mut self, _x:usize, _y:usize) {
-        self.b[_y][_x] = 1;
+    pub fn set_board(&mut self, _x:usize, _y:usize, _value:i32) {
+        self.b[_y][_x] = _value;
     }
 
     pub fn move_king(&mut self, _x:i32, _y:i32) {
--- GameServer/src/game.rs
+++ GameServer/src/game.rs
@@ -12,7 +12,10 @@ impl _Game {
     }
 
     pub fn game_loop(&mut self) {
-        self._b.set_board(4, 4);
+        self._b.set_board(4, 4, 1);
+        self._b.show_board();
+        println!();
+        self._b.move_king(5,5);
         self._b.show_board();
     }
 }
